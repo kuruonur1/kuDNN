f69a7f1874950ab97622bf4f01f1cd6fdf26c633
Onur Kuru <kuru.onur1@gmail.com>
2016-02-02 10:44:53 +0200

    test2 updated according to new api

--- test/jul/test2.jl
+++ test/jul/test2.jl
@@ -1,21 +1,21 @@
 using CUDNN: cudnnConvolutionBackwardBias, cudnnConvolutionBackwardFilter, cudnnConvolutionBackwardData, cudnnConvolutionForward
-using CUDNN: ConvolutionDescriptor, CUDNN_CROSS_CORRELATION, CUDNN_CONVOLUTION
+using CUDNN: CD, CUDNN_CROSS_CORRELATION, CUDNN_CONVOLUTION
 using CUDArt
 
 
 # x = reshape(Float64[1:9],3,3,1,1); tx = CudaArray(x)
 # w = reshape(Float64[1:4],2,2,1,1); tw = CudaArray(w)
 pads = (0,0); strides = (2,2)
-corrd = ConvolutionDescriptor(padding=pads, stride=strides, mode=CUDNN_CROSS_CORRELATION)
-x = float64(rand(1:10,5,5,1,1)); tx = CudaArray(x)
-w = float64(rand(1:10,2,2,1,1)); tw = CudaArray(w)
+corrd = CD(padding=pads, stride=strides, mode=CUDNN_CROSS_CORRELATION)
+x = rand(Float64[1:10;],5,5,1,1); tx = CudaArray(x)
+w = rand(Float64[1:10;],2,2,1,1); tw = CudaArray(w)
 @show size(x)
 @show size(w)
 @show pads
 @show strides
 ty = cudnnConvolutionForward(tx, tw;convDesc=corrd); y = to_host(ty)
 @show size(y)
-dy = float64(rand(1:10,size(ty))); tdy = CudaArray(dy)
+dy = rand(Float64[1:10;],size(ty)); tdy = CudaArray(dy)
 # dy = reshape(Float64[5 6 7 8],2,2,1,1); tdy = CudaArray(dy) #2x2
 println(x)
 println(w)
@@ -32,10 +32,10 @@ cudnnConvolutionBackwardData(tw, tdy, tdx; convDesc=corrd)
 println(to_host(tdx))
 
 println("testing pooling..")
-using CUDNN: cudnnPoolingForward, cudnnPoolingBackward, cudnnGetPoolingNdForwardOutputDim, PoolingDescriptor, CUDNN_POOLING_MAX
+using CUDNN: cudnnPoolingForward, cudnnPoolingBackward, cudnnGetPoolingNdForwardOutputDim, PD, CUDNN_POOLING_MAX
 # x = reshape(Float64[5 2 3 4 5 6 7 7 3 1 4 5], 3, 4, 1, 1); tx = CudaArray(x)
-x = float64(rand(1:10,10,10,1,1)); tx = CudaArray(x)
+x = rand(Float64[1:10;],10,10,1,1); tx = CudaArray(x)
 println("x:"); println(x)
-pd2 = PoolingDescriptor((3,3); padding=(0,0), stride=(5,5), mode=CUDNN_POOLING_MAX)
+pd2 = PD(2,3,0,1, CUDNN_POOLING_MAX)
 ydims = cudnnGetPoolingNdForwardOutputDim(pd2, tx)
 @show ydims
